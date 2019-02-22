3af8dddabe16dadc0b4e79611668d71386d682bd
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-02-22 14:12:49 -0800

    Add event listener for number buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,11 @@
 const numberButtons = document.querySelectorAll('.numberButton');
 const submitButton = document.querySelector('.submitButton');
 const clearButton = document.querySelector('.clearButton');
+let pokemonNameDisplay = document.querySelector('.pokemonNameDisplay');
 let pokemonInput = document.querySelector("input");
+const pokeAPI = 'https://pokeapi.co/api/v2/pokemon/';
+var request = new XMLHttpRequest();
+
 
 pokedexListener();
 
@@ -23,3 +27,36 @@ function clearData(){
 clearButton.addEventListener("click", function(){
   clearData();
 })
+
+// function submitData(){
+//   request.open('GET', pokeAPI + pokemonInput.value, true);
+//   request.onload = function () {
+//
+//     // Begin accessing JSON data here
+//     var data = JSON.parse(this.response);
+//     if (request.status >= 200 && request.status < 400) {
+//       pokemonNameDisplay.textContent = name;
+//       // data.forEach(movie => {
+//       //   const card = document.createElement('div');
+//       //   card.setAttribute('class', 'card');
+//       //
+//       //   const h1 = document.createElement('h1');
+//       //   h1.textContent = movie.title;
+//       //
+//       //   const p = document.createElement('p');
+//       //   movie.description = movie.description.substring(0, 300);
+//       //   p.textContent = `${movie.description}...`;
+//       //
+//       //   container.appendChild(card);
+//       //   card.appendChild(h1);
+//       //   card.appendChild(p);
+//       // });
+//     } else {
+//       const errorMessage = document.createElement('marquee');
+//       errorMessage.textContent = `Gah, it's not working!`;
+//       app.appendChild(errorMessage);
+//     }
+//   }
+//
+//   request.send();
+// }
